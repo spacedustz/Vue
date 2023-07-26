7ede671d065ac8de364a901ab5b2c9806b8e434c
owen <wavefunctionc@gmail.com>
2023-07-26 18:44:39 +0900

    Without JS + USe Vue

---
+++ without-js/app.js
@@ -0,0 +1,42 @@
+// // Query Selector를 통해 HTML 요소의 Javascript 객체 구현을 가리키는 포인터를 상수에 할당합니다.
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// // 이 함수는 사용자가 입력한 값을 받아서 Goal을 수정해줍니다.
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+
+//   // Javascript로 새 DOM 요소를 프로그래밍 방식으로 생성
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+
+//   // 프로그래밍 방식으로 생성한 listItemEl을 Child로 추가
+//   listEl.appendChild(listItemEl);
+
+//   // 이전의 Goal들 삭제
+//   inputEl.value = '';
+// }
+
+// // ButtonEl에 Clien Listener 추가해서 버튼에 접근하여 클릭 이벤트 수신
+// // 두번째 파라미터는 클릭이 발생할 때 실행할 함수 지정
+// buttonEl.addEventListener('click', addGoal);
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+
+  // 이 부분
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+
+      // 입력값 리셋이 필요할 경우
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app');
