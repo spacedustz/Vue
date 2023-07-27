f838bdd98046a02bce1d17b0ef3f7ac690c199e1
owen <floralexium@gmail.com>
2023-07-27 14:44:44 +0900

    Add Vue Methods

--- 2-Start-Vue/app.js
+++ 2-Start-Vue/app.js
@@ -4,6 +4,19 @@ const app = Vue.createApp({
       courseGoal: 'Finish the course and learn Vue!',
       vueLink: 'https://vuejs.org/'
     };
+  },
+
+  // 이 Methods의 값인 객체의 프로퍼티는 함수여야 합니다.
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.ramdom();
+
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    }
   }
 });
 
