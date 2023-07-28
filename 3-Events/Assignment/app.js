361b881b50cdb14747297346b647a2add1599578
owen <wavefunctionc@gmail.com>
2023-07-28 19:26:51 +0900

    Section3-과제-완료

---
+++ 3-Events/Assignment/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return { userInput: '', confirmedInput: '' };
+  },
+
+  methods: {
+    showAlert() {
+      alert('This works!');
+    },
+    saveInput(event) {
+      this.userInput = event.target.value;
+    },
+    confirmInput() {
+      this.confirmedInput = this.userInput;
+    }
+  }
+});
+
+app.mount('#assignment')
