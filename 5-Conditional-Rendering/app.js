8751a715581e922e283fba5b9b0a8f527cb25670
KunWoo Shin <cyberspecterr@gmail.com>
2023-07-31 18:09:54 +0900

    Update Conditional Rendering

--- 5-Conditional-Rendering/app.js
+++ 5-Conditional-Rendering/app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      enteredGoalValue: '',
+      goals: [] 
+    };
   },
+
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = '';
+    }
+  }
 });
 
 app.mount('#user-goals');
