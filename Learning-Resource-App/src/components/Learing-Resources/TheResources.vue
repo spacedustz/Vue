1ebe97a7d3630d459b63f991db2709751198e05b
owen <floralexium@gmail.com>
2023-08-05 16:51:43 +0900

    Add Base Button

---
+++ Learning-Resource-App/src/components/Learing-Resources/TheResources.vue
@@ -0,0 +1,18 @@
+<template>
+  <base-card>
+    <base-button>Stored Re</base-button>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from "@/components/UI/BaseCard";
+import BaseButton from "@/components/UI/BaseButton";
+export default {
+  name: "TheResources",
+  components: {BaseButton, BaseCard}
+}
+</script>
+
+<style scoped>
+
+</style>
