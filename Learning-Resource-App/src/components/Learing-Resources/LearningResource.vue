f701baed6e643c16564ddaf0f94c5033266ed99f
owen <wavefunctionc@gmail.com>
2023-08-05 16:31:14 +0900

    Add Styling + UI + Layout Components

--- Learning-Resource-App/src/components/Learing-Resources/LearningResource.vue
+++ Learning-Resource-App/src/components/Learing-Resources/LearningResource.vue
@@ -1,17 +1,15 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-
-    <p>{{ description }}</p>
-
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
@@ -22,6 +20,34 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
 
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
 </style>
