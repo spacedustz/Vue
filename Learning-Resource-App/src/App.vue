6b3cfb37c4b7a325440cb9398b44487745d7c956
spacedustz <spacedustw@gmail.com>
2023-08-05 15:24:30 +0900

    데미 데이터 추가

---
+++ Learning-Resource-App/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <learning-resource
+        v-for="res in storedResources"
+        :key="res.id"
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/Learing-Resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResource
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js Documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The Official Google Documentation',
+          link: 'https://google.org'
+        },
+      ]
+    };
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
