f51ff64a7f76e186681b9cda498fae676f871b97
owen <wavefunctionc@gmail.com>
2023-08-05 14:30:33 +0900

    Add TypeScript Vue Project

---
+++ Vue-TS/src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" class="form-control" placeholder="할일을 입력해주세요." />
+</template>
+<script lang="ts">
+export default {
+  name: "TodoInput",
+};
+
+</script>
+<script lang="ts" setup></script>
+
+<style lang="scss">
+.todo-input {
+  font-size: 14px;
+
+  &::placeholder {
+    font-size: 14px;
+  }
+}
+</style>
---
+++ Vue-TS/src/views/ItemList.vue
@@ -0,0 +1,12 @@
+<template>
+  <todo-item />
+</template>
+<script lang="ts">
+export default {
+  name: "ItemList",
+};
+</script>
+<script lang="ts" setup>
+import TodoItem from "@/components/Item.vue";
+</script>
+<style lang="scss"></style>
