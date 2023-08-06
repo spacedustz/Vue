7a012bf067d88d80e7d1252e5ae414cddabe1c5c
owen <wavefunctionc@gmail.com>
2023-08-07 03:12:02 +0900

    Add Form V-Model

---
+++ Forms/src/components/RatingControl.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li><button>Poor</button></li>
+    <li><button>Average</button></li>
+    <li><button>Great</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RatingControl"
+}
+</script>
+
+<style scoped>
+
+</style>
