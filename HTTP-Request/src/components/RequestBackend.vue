a66e70a566fec49e7621d83b610d6b1a15c25a9d
owen <wavefunctionc@gmail.com>
2023-08-15 16:04:05 +0900

    Rest API 데이터 받아오는 로직 리팩터링

--- HTTP-Request/src/components/RequestBackend.vue
+++ HTTP-Request/src/components/RequestBackend.vue
@@ -6,10 +6,10 @@
         <h3>Event ID: {{ item.id }}</h3>
         <p>Frame ID: {{ item.frameId }}</p>
 
-        <div v-if="item.extra">
+        <div v-if="itemHasExtra(item)">
           <h4>Extra Information</h4>
-          <p>Bbox Height : {{ item.extra.bbox.height }}</p>
-          <p>Bbox Width : {{ item.extra.bbox.width }}</p>
+          <p>Bbox Height: {{ getBboxHeight(item.extra) }}</p>
+          <p>Bbox Width: {{ getBboxWidth(item.extra) }}</p>
 
           <h4>Vertices</h4>
           <ul>
@@ -25,31 +25,34 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "RequestBackend"
-}
-</script>
-
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-// 데이터를 저장할 리액티브 변수 생성
 const items = ref([]);
 
-// 데이터 가져오는 함수
 const fetchData = async () => {
   try {
     const response = await axios.get('http://localhost:8081/api/json', { withCredentials: true });
-    items.value = response.data; // 데이터를 리액티브 변수에 저장
+    items.value = response.data;
     console.log(response.data);
   } catch (error) {
     console.error('데이터를 가져오는 동안 오류 발생:', error);
   }
 };
 
-// 컴포넌트가 마운트되면 데이터 가져오기 함수 실행
+const itemHasExtra = (item) => {
+  return !!item.extra;
+};
+
+const getBboxHeight = (extra) => {
+  return extra.bbox.height;
+};
+
+const getBboxWidth = (extra) => {
+  return extra.bbox.width;
+};
+
 onMounted(() => {
   fetchData();
 });
