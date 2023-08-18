85ff3ef00b168c7b8a895c1ea3b28b850c2d39d3
owen <wavefunctionc@gmail.com>
2023-08-18 15:13:45 +0900

    Refactor RTSP to HLS Streaming

--- HTTP-Request/src/components/StreamingRtsp.vue
+++ HTTP-Request/src/components/StreamingRtsp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="video" align="center">
-    <video id="video" ref="videoPlayer" controls="controls" autoplay muted>
+    <video id="video" controls="controls" autoplay muted>
       <source :src="videoUrl" type="application/x-mpegURL" />
       Your browser does not support HTML5 streaming!
     </video>
@@ -19,41 +19,40 @@ import axios from 'axios';
 import Hls from 'hls.js';
 
 const videoUrl = ref('');
-const videoPlayer = ref(null);
-const hlsInstance = ref(null);
+const baseUrl = 'http://localhost:18080'
 
 const fetchVideo = async () => {
   try {
-    const response = await axios.get('http://localhost:8081/api/video', { withCredentials: true });
-    // videoUrl.value = response.data;
-    initVideoPlayer();
+    const response = await axios.get(`${baseUrl}/api/video`, { withCredentials: true });
+    videoUrl.value = baseUrl + response.data;
+    console.log('Video URL:', videoUrl.value); // #EXT ~~
+
+    const videoElement = document.getElementById('video');
+    console.log("is Supported ? = " + Hls.isSupported());
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+
+      // // Set up a data URI with the m3u8 content received from the backend
+      // const blob = new Blob([videoUrl.value], { type: 'application/x-mpegurl' });
+      // const objectURL = URL.createObjectURL(blob);
+      // console.log('Object URL' + objectURL);
+      hls.loadSource(videoUrl.value); // Undefined
+      hls.attachMedia(videoElement);
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        console.log("Manifest_Passed Event 발생")
+        videoElement.play();
+      });
+    } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+      videoElement.src = videoUrl.value;
+      videoElement.play();
+    }
+
     console.log(response.data);
   } catch (error) {
-    console.error('비디오 가져오기 실패:', error);
+    console.error('비디오 가져오기 실패:', error.message);
   }
 }
-
-const initVideoPlayer = () => {
-  const videoElement = videoPlayer.value;
-  console.log('Element = ' + videoElement);
-
-  console.log("is Supported ? = " + Hls.isSupported());
-  if (Hls.isSupported()) {
-    const hls = new Hls();
-    hlsInstance.value = hls;
-
-    // // Set up a data URI with the m3u8 content received from the backend
-    // const blob = new Blob([videoUrl.value], { type: 'application/x-mpegurl' });
-    // const objectURL = URL.createObjectURL(blob);
-    // console.log('Object URL' + objectURL);
-
-    hls.loadSource(videoUrl.value);
-    hls.attachMedia(videoElement);
-    hls.on(Hls.Events.MANIFEST_PARSED, function () { videoElement.play(); });
-  } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
-    videoElement.src = videoUrl.value;
-  }
-};
 
 onMounted(() => {
   fetchVideo();
