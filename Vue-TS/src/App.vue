feb283bed2bcc04a0fc6638a922c6bcf89c79e75
owen <wavefunctionc@gmail.com>
2023-08-19 15:35:20 +0900

    Move Logics

--- Vue-TS/src/App.vue
+++ Vue-TS/src/App.vue
@@ -1,64 +1,22 @@
-<!-- BootStrap Grid 사용 -->
 <template>
-  <div class="container">
+  <div>
+    <DummyComponent></DummyComponent>
+  </div>
 
-    <div class="row">
-      <base-header />
-    </div>
+  <div>
+    <RequestBackend></RequestBackend>
+  </div>
 
-    <!-- emit을 통해 이벤트(update:todo)가 발생하면 할일 목록 추가 -->
-    <div class="row">
-      <todo-input @update:todo="handleUpdateTodoList" />
-    </div>
-
-    <!-- 할일 목록 바인딩 -->
-    <div class="row">
-      <router-view :todoList="todoList" />
-    </div>
-
+  <div>
+    <streaming-hls></streaming-hls>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import BaseHeader from "./components/Header.vue";
-import TodoInput from "./components/Input.vue";
 
-interface TodoItem {
-  id: number;
-  title: string;
-  status: "active" | "clear";
-}
-
-// 할일 목록 TodoItem 배열 타입으로 형변환
-const todoList = ref([
-  {
-    id: 0,
-    title: "청소하기",
-    status: "active"
-  },
-  {
-    id: 1,
-    title: "공과금 내기",
-    status: "active"
-  },
-  {
-    id: 2,
-    title: "운동 1시간 하기",
-    status: "clear"
-  },
-] as TodoItem[]);
-
-// TodoItem 타입의 item 파라미터를 받아서 todoList에 항목 추가
-const handleUpdateTodoList = (item: TodoItem) => { todoList.value.push(item) };
+import DummyComponent from "@/components/DummyComponent.vue";
+import RequestBackend from "@/components/RequestBackend.vue";
+import StreamingHls from "@/components/StreamingHls.vue";
 </script>
 
-<style lang="scss">
-#app {
-  padding: 50px 0;
-
-  .row + .row {
-    margin-top: 15px;
-  }
-}
-</style>
+<style></style>
