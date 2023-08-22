805f1e2f3055ea3e2896a1bdf6c7710ce73db6f4
owen <wavefunctionc@gmail.com>
2023-08-22 11:37:30 +0900

    Time Graph 수정

--- Vue-TS/src/components/TimeGraph.vue
+++ Vue-TS/src/components/TimeGraph.vue
@@ -5,16 +5,18 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import {ref, onMounted, watchEffect } from 'vue';
 import { Line } from 'vue-chartjs';
-import axios from 'axios';
+import { fetchFrame } from "@/services/ApiService";
+
+const frameData = ref<Array<any>>([]);
 
 const chartData = ref({
-  labels: [],
+  labels: frameData.map(data => data.frameId),
   datasets: [
     {
       label: '프레임 카운트',
-      data: [],
+      data: ['count', 'frame_id', 'frame_time', 'instance_id', 'system_date', 'system_timestamp'],
       borderColor: 'rgba(75, 192, 192, 1)',
       fill: false,
     },
@@ -26,16 +28,19 @@ const chartOptions = ref({
   maintainAspectRatio: false,
 });
 
+// 주기적으로 Watch를 걸고 데이터가 변경될 경우 frameData를 업데이트 함
+// 이 방법은 API에 많은 부담을 줄 수 있으므로, 실제 프로덕트에서는 웹소켓이나 다른 방법 권장
+watchEffect(async () => {
+  setInterval(async () => {
+    frameData.value = await fetchFrame();
+  }, 10000);
+})
+
 onMounted(async () => {
   try {
-    const response = await axios.get('/api/csv'); // 실제 API 엔드포인트로 대체하세요
+    frameData.value = await fetchFrame();
     const frames = response.data;
 
-    const labels = frames.map((frame) => frame.systemDate); // 'systemDate'가 필드 이름인 것으로 가정
-    const data = frames.map((frame) => frame.count);
-
-    chartData.value.labels = labels;
-    chartData.value.datasets[0].data = data;
   } catch (error) {
     console.error('데이터를 가져오는 중 오류 발생:', error);
   }
