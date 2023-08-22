eeef690d1bf56cbcc2b677a179b9803eab90745f
owen <floralexium@gmail.com>
2023-08-22 17:43:25 +0900

    라우터에 컴포넌트 추가

--- Vue-TS/src/router/index.ts
+++ Vue-TS/src/router/index.ts
@@ -1,8 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RequestBackend from '../components/RequestBackend.vue'
+import StreamingHls from '../components/StreamingHls.vue'
+import TimeGraph from '../components/TimeGraph.vue'
+import {DoughnutChart} from "vue-chart-3";
+
+const routes = [
+  {
+    path: '/json',
+    name: 'RequestBackend',
+    component: RequestBackend
+  },
+  {
+    path: '/video',
+    name: 'StreamingHls',
+    component: StreamingHls
+  },
+  {
+    path: '/graph',
+    name: 'TimeGraph',
+    component: TimeGraph
+  },
+  {
+    path: '/test',
+    name: 'Doughnut Chart',
+    component: DoughnutChart
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes
 })
 
 export default router
