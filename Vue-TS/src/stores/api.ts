807ab2c96c805a0b695a027104a029d7022b4230
owen <wavefunctionc@gmail.com>
2023-08-22 17:43:05 +0900

    Axios Rest API 1개 추가

--- Vue-TS/src/stores/api.ts
+++ Vue-TS/src/stores/api.ts
@@ -10,11 +10,20 @@ export const fetchFrame = async (): Promise<any[]> => {
     }
 }
 
-export const fetchJson = async (): Promise<string | undefined> => {
+export const fetchVideo = async (): Promise<string> => {
     try {
         const response = await axios.get('http://localhost:18080/api/video');
         return response.data;
     } catch (error) {
         console.error('비디오 가져오기 실패:', error);
+    }
+}
+
+export const fetchJson=  async (): Promise<any> => {
+    try {
+        const response = await axios.get('http://localhost:18080/api/json')
+        return response.data;
+    } catch (error) {
+        console.error('Json 데이터 가져오기 실패', error);
     }
 }
