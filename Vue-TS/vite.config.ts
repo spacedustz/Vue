b080e40a4cae5675e85c4665be23429ced9ff98f
owen <wavefunctionc@gmail.com>
2023-08-22 17:41:45 +0900

    vite 설정 변경

--- Vue-TS/vite.config.ts
+++ Vue-TS/vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // HLS 추가
+  optimizeDeps: {
+    include: ['hls.js']
   }
 })
